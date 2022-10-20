bc94e8458f9b325434d4c9f413689fa0532c1b09
Ray Young <ray.arden.young@gmail.com>
2022-10-19 20:06:39 -0400

    linked empty album detail page

---
+++ music/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Album
+
+# Create your views here.
+
+def index(request):
+    albums = Album.objects.all()
+    return render(request, 'music/index.html', {'albums': albums})
+
+def album_detail(request):
+    albums = Album.objects.all()
+    return render(request, 'music/album_detail.html', {'albums': albums})
